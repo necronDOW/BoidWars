ec1b6833e24fd1457722c1ee1210123d22de1944
Liam Wilson <liamryanwilson@hotmail.co.uk>
2017-01-24 01:29:41 +0000

    #3 Create Level Painting Tool
    Added a tool for generating static colliders on a series of objects.

---
+++ Assets/Scripts/Editor/GenerateCollider_Editor.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEditor;
+
+public class SerializedPropertyExt
+{
+    public static T GetObject<T>(SerializedProperty property) where T : class
+    {
+        SerializedObject serializedObject = property.serializedObject;
+        if (serializedObject == null)
+        {
+            return null;
+        }
+
+        Object targetObject = serializedObject.targetObject;
+        object obj = targetObject.GetType().GetField(property.name).GetValue(targetObject);
+        if (obj == null)
+        {
+            return null;
+        }
+        
+        T actualObject = null;
+        if (obj.GetType().IsArray)
+        {
+            int index = ((object[])obj).Length - 1;
+
+            if (index >= 0)
+            {
+                actualObject = ((T[])obj)[0];
+            }
+        }
+        else
+        {
+            actualObject = obj as T;
+        }
+
+        return actualObject;
+    }
+    public static T GetObject<T>(SerializedObject serializedObject) where T : class
+    {
+        Object targetObject = serializedObject.targetObject;
+        object obj = targetObject;
+        if (obj == null)
+        {
+            return null;
+        }
+
+        T actualObject = null;
+        if (obj.GetType().IsArray)
+        {
+            int index = ((object[])obj).Length - 1;
+
+            if (index >= 0)
+            {
+                actualObject = ((T[])obj)[0];
+            }
+        }
+        else
+        {
+            actualObject = obj as T;
+        }
+
+        return actualObject;
+    }
+}
+
+[CustomEditor(typeof(GenerateCollider))]
+public class GenerateCollider_Editor : Editor
+{
+    private GenerateCollider gc;
+
+    private void OnEnable()
+    {
+        gc = (GenerateCollider)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        // List drag & drop event
+        EditorGUI.BeginChangeCheck();
+        SerializedProperty targets = serializedObject.FindProperty("targets");
+        EditorGUILayout.PropertyField(targets, true);
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            serializedObject.ApplyModifiedProperties();
+
+            Transform parent = SerializedPropertyExt.GetObject<Transform>(targets);
+            if (parent && !parent.GetComponent<MeshFilter>())
+            {
+                AddForChildren(SerializedPropertyExt.GetObject<Transform>(targets), gc);
+            }
+        }
+        
+        // Generate Colliders button press
+        GUILayout.Space(5);
+        if (GUILayout.Button("Update Colliders"))
+        {
+            gc.UpdateColliders();
+        }
+
+        if (gc.targets != null)
+        {
+            int colliderDifference = gc.targets.Length - gc.colliderCount;
+            if (colliderDifference <= 0)
+            {
+                for (int i = 0; i < (colliderDifference * -1); i++)
+                {
+                    gc.DeleteCollider(gc.colliders.Count - 1);
+                }
+
+                gc.colliderCount += colliderDifference;
+            }
+        }
+
+        // Show colliders
+        //if (GUILayout.Button(gc.VisibleText()))
+        //{
+        //    gc.ToggleCollidersVisible();
+        //}
+    }
+
+    private void AddForChildren(Transform parent, GenerateCollider gc)
+    {
+        if (parent && parent.childCount > 0)
+        {
+            int gcLenModified = gc.targets.Length - 1;
+
+            Transform[] newTargets = new Transform[gcLenModified + parent.childCount];
+
+            for (int i = 0; i < gcLenModified; i++)
+            {
+                newTargets[i] = gc.targets[i + 1];
+            }
+
+            for (int i = 0; i < parent.childCount; i++)
+            {
+                newTargets[gcLenModified + i] = parent.GetChild(i);
+            }
+
+            gc.targets = newTargets;
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Assets/Scripts/GenerateCollider.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class GenerateCollider : MonoBehaviour
+{
+    public Transform[] targets;
+    [HideInInspector]
+    [SerializeField]
+    public int colliderCount = 0;
+    [HideInInspector]
+    public List<BoxCollider> colliders = new List<BoxCollider>();
+
+    private bool _visible = false;
+
+    private void Awake()
+    {
+        foreach (Transform t in targets)
+        {
+            MeshFilter tMeshFilter = t.GetComponent<MeshFilter>();
+
+            if (t && tMeshFilter && tMeshFilter.mesh.vertexCount > 0)
+            {
+                Transform child = new GameObject(t.name + "_collider").transform;
+                child.parent = t;
+
+                BoxCollider bc = child.gameObject.AddComponent<BoxCollider>();
+                bc.size = GetDimensions(t);
+
+                child.localPosition = new Vector3(0f, 0f, bc.size.z / 2.0f);
+                child.localEulerAngles = Vector3.zero;
+            }
+        }
+    }
+
+    public void UpdateColliders()
+    {
+        for (int i = colliderCount; i < targets.Length; i++)
+        {
+            MeshFilter tMeshFilter = targets[i].GetComponent<MeshFilter>();
+
+            if (targets[i] && tMeshFilter && tMeshFilter.sharedMesh.vertexCount > 0)
+            {
+                Transform child = new GameObject(targets[i].name + "_collider").transform;
+                child.parent = targets[i];
+
+                BoxCollider bc = child.gameObject.AddComponent<BoxCollider>();
+                bc.size = GetDimensions(targets[i]);
+
+                child.localPosition = new Vector3(0f, 0f, bc.size.z / 2.0f);
+                child.localEulerAngles = Vector3.zero;
+
+                child.transform.parent = transform;
+
+                colliders.Add(bc);
+            }
+        }
+
+        //UpdateCollidersVisible(colliderCount);
+
+        colliderCount = colliders.Count;
+    }
+
+    public void DeleteCollider(int index)
+    {
+        if (index >= 0 && index < colliders.Count)
+        {
+            if (colliders[index])
+            {
+                DestroyImmediate(colliders[index].gameObject);
+            }
+            colliders.RemoveAt(index);
+        }
+    }
+
+    private void UpdateCollidersVisible(int start = 0)
+    {
+        HideFlags flag = _visible ? HideFlags.None : HideFlags.HideInHierarchy;
+
+        for (int i = start; i < colliders.Count; i++)
+        {
+            colliders[i].gameObject.hideFlags = flag;
+        }
+    }
+
+    public void ToggleCollidersVisible()
+    {
+        _visible = !_visible;
+        UpdateCollidersVisible();
+    }
+
+    public string VisibleText()
+    {
+        return _visible ? "Hide Colliders" : "Show Colliders";
+    }
+
+    private Vector3 GetDimensions(Transform t)
+    {
+        Vector3 min = Vector3.one * Mathf.Infinity; 
+        Vector3 max = Vector3.one * Mathf.NegativeInfinity;
+
+        Mesh m = t.GetComponent<MeshFilter>().sharedMesh;
+        foreach (Vector3 v in m.vertices)
+        {
+            min = Vector3.Min(min, v);
+            max = Vector3.Max(max, v);
+        }
+
+        return Vector3.Scale(max-min, t.localScale);
+    }
+}
